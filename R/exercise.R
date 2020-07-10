103f910c69ed7d899b364d3a34b9c3c6369a8e86
ariannabienati <abienati@smail.uni-koeln.de>
2020-07-10 09:34:59 +0200

    exercise 10

--- R/exercise.R
+++ R/exercise.R
@@ -26,3 +26,27 @@ df <- data.frame(
   blue=unlist(lapply(corpus, wc, p="\\b(blue)\\b"))
 )
 
+#a
+
+barplot(df$tokens, xlab="documents", ylab="tokens")
+
+#b
+
+green.doc <- (df$green) > 0
+vec.doc.green <- df$green[green.doc] 
+blue.green.red.doc <- ((df$green)|(df$red)|(df$blue))
+df.colors <- rbind(df$green, df$red, df$blue)
+vec.doc.colors <- df.colors[, blue.green.red.doc]
+
+barplot(vec.doc.colors, col=c("green", "red", "blue"), xlab="documents", ylab="colors")
+
+#c
+
+heatmap(as.matrix(dist(df, "manhattan")), Colv = NA, Rowv = NA)
+
+#d/e
+mat <- matrix(c(1,2), byrow=TRUE, nrow = 1, ncol = 2)
+layout(mat, widths = c(2.5, 0.5))
+barplot(df$sentences, ylim=c(0, 1600), yaxs="i", xlab="Documents", ylab="Sentences")
+boxplot(df$sentences, ylim=c(0, 1600), yaxs="i", frame = FALSE)
+
